7169b55a8f549f258efd42ecfd113f7fbc67373a
BernhardBaumrock <office@baumrock.com>
2023-04-19 15:30:29 +0200

    change host

--- site/config-local.php
+++ site/config-local.php
@@ -11,7 +11,7 @@ $config->dbPass = 'db';
 $config->dbHost = 'db';
 $config->userAuthSalt = '3177514880a112f85e7a89636bf09aa0ddb4b8e8';
 $config->tableSalt = '9fa1e33c8cf6d18e90a49506b407d8524d33c6ed';
-$config->httpHosts = ['meetup.ddev.site'];
+$config->httpHosts = ['meetupwall.ddev.site'];
 $config->sessionFingerprint = false;
 
 // RockFrontend
