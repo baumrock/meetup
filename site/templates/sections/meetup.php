053ee1b13fe590f92f39d2b0f6be359106705577
BernhardBaumrock <office@baumrock.com>
2023-04-19 18:45:28 +0200

    add link back to overview

--- site/templates/sections/meetup.php
+++ site/templates/sections/meetup.php
@@ -1,9 +1,15 @@
 <div class="uk-margin">@ <?= $page->location ?></div>
 
 <?php if ($page->image) : ?>
-  <div class="uk-section uk-text-center" uk-lightbox>
+  <div class="uk-section-small uk-text-center" uk-lightbox>
     <a href="<?= $page->image->maxSize(1920, 1920)->webp->url ?>">
       <img src="<?= $page->image->maxSize(400, 300)->webp->url ?>">
     </a>
   </div>
 <?php endif; ?>
+
+<div class="uk-section-small uk-text-center">
+  <a href="<?= $page->parent->url ?>">
+    &lt; Back to all Meetups
+  </a>
+</div>
