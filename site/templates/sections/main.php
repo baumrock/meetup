30309fab69e6b5c8d3a5d488f77494606f0d7f9d
BernhardBaumrock <office@baumrock.com>
2023-04-19 18:15:42 +0200

    add breadcrumbs

--- site/templates/sections/main.php
+++ site/templates/sections/main.php
@@ -1,5 +1,10 @@
 <main>
   <div class="rf-box uk-margin-top">
+    <div>
+      <?php foreach ($page->parents() as $item) : ?>
+        <a href="<?= $item->url ?>"><?= $item->title ?></a> /
+      <?php endforeach; ?>
+    </div>
     <h1><?= $page->title ?></h1>
     <?= $rockfrontend->renderIf("sections/wall.php", $page->template == 'wall'); ?>
   </div>
