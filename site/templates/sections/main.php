63f39d4a967d2081bb3bd15d80b085e080622bb9
BernhardBaumrock <office@baumrock.com>
2023-04-19 18:15:25 +0200

    fix h1 margin

--- site/templates/sections/main.php
+++ site/templates/sections/main.php
@@ -1,5 +1,5 @@
 <main>
-  <div class="rf-box">
+  <div class="rf-box uk-margin-top">
     <h1><?= $page->title ?></h1>
     <?= $rockfrontend->renderIf("sections/wall.php", $page->template == 'wall'); ?>
   </div>
