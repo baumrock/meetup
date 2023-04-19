c2251fb7bb9d3e1cbd5f7c7e43bb949bd53f6553
BernhardBaumrock <office@baumrock.com>
2023-04-19 18:32:47 +0200

    fix error if no image

--- site/templates/sections/meetup.php
+++ site/templates/sections/meetup.php
@@ -1,7 +1,9 @@
 <div class="uk-margin">@ <?= $page->location ?></div>
 
-<div class="uk-section uk-text-center" uk-lightbox>
-  <a href="<?= $page->image->maxSize(1920, 1920)->webp->url ?>">
-    <img src="<?= $page->image->maxSize(400, 300)->webp->url ?>">
-  </a>
-</div>
+<?php if ($page->image) : ?>
+  <div class="uk-section uk-text-center" uk-lightbox>
+    <a href="<?= $page->image->maxSize(1920, 1920)->webp->url ?>">
+      <img src="<?= $page->image->maxSize(400, 300)->webp->url ?>">
+    </a>
+  </div>
+<?php endif; ?>
